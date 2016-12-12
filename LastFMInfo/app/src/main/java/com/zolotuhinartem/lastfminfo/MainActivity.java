1358f846c3b03f4c0e4c43add5c8b5c687e3f008
Zolotuhin Artem <miktalantlikytli@gmail.com>
2016-12-12 16:09:15 +0300

    zolotuhinartem: first commit

---
+++ LastFMInfo/app/src/main/java/com/zolotuhinartem/lastfminfo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.zolotuhinartem.lastfminfo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnFind;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFind = (Button) findViewById(R.id.btn_activity_main_find);
+        btnFind.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_activity_main_find:
+                Intent intent = new Intent(this, FindActivity.class);
+                startActivity(intent);
+        }
+    }
+}
