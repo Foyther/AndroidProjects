31ded09a1c291f6eb4c0093633d35be71b29e64c
zolotuhinartem <miktalantlikytli@gmail.com>
2016-12-30 13:16:44 +0300

    ZAP: Redisign: Coordinatorlayout and collapselayout in album info, toolbar under status bar, coordinatorlayout everywhere

--- LastFMInfo/app/src/main/java/com/zolotuhinartem/lastfminfo/activities/top_tracks/TopTracksActivity.java
+++ LastFMInfo/app/src/main/java/com/zolotuhinartem/lastfminfo/activities/top_tracks/TopTracksActivity.java
@@ -1,4 +1,4 @@
-package com.zolotuhinartem.lastfminfo.activities;
+package com.zolotuhinartem.lastfminfo.activities.top_tracks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ LastFMInfo/app/src/main/java/com/zolotuhinartem/lastfminfo/activities/track_info/TrackInfoActivity.java
@@ -0,0 +1,15 @@
+package com.zolotuhinartem.lastfminfo.activities.track_info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zolotuhinartem.lastfminfo.R;
+
+public class TrackInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_info);
+    }
+}
