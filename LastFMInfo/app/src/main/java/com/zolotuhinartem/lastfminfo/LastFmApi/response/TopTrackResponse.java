ed3ee7794ab81d4715a7e2b7c489a184cb9a8579
zolotuhinartem <miktalantlikytli@gmail.com>
2017-01-05 16:35:54 +0300

    ZAP: Top Track Pojo special for Nurislam

---
+++ LastFMInfo/app/src/main/java/com/zolotuhinartem/lastfminfo/LastFmApi/response/TopTrackResponse.java
@@ -0,0 +1,22 @@
+
+package com.zolotuhinartem.lastfminfo.LastFmApi.response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.zolotuhinartem.lastfminfo.LastFmApi.response.pojo.top_tracks.Tracks;
+
+public class TopTrackResponse {
+
+    @SerializedName("tracks")
+    @Expose
+    private Tracks tracks;
+
+    public Tracks getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(Tracks tracks) {
+        this.tracks = tracks;
+    }
+
+}
